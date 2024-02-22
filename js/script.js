fb5b9eb0804233e1189abf651eeffd0986fac685
Lukas G <luka9938@stud.kea.dk>
2024-02-22 09:09:26 +0100

    DOM loaded not needed

--- js/script.js
+++ js/script.js
@@ -1,24 +1,22 @@
 'use strict';
 
-document.addEventListener("DOMContentLoaded", function() {
-    const form = document.getElementById("formContact");
-    const tbody = document.querySelector("#list tbody");
+const form = document.getElementById("formContact");
+const tbody = document.querySelector("#list tbody");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
     
-    form.addEventListener("submit", function(event) {
-        event.preventDefault();
-        
-        const author = document.getElementById("txtAuthor").value;
-        const title = document.getElementById("txtTitle").value;
-        const year = document.getElementById("txtYear").value;
-        
-        const template = document.getElementById("album-list").content.cloneNode(true);
-        
-        template.querySelector("[data-field='authors']").textContent = author;
-        template.querySelector("[data-field='titles']").textContent = title;
-        template.querySelector("[data-field='years']").textContent = year;
-        
-        tbody.appendChild(template);
-        
-        form.reset();
-    });
+    const author = document.getElementById("txtAuthor").value;
+    const title = document.getElementById("txtTitle").value;
+    const year = document.getElementById("txtYear").value;
+    
+    const template = document.getElementById("album-list").content.cloneNode(true);
+    
+    template.querySelector("[data-field='authors']").textContent = author;
+    template.querySelector("[data-field='titles']").textContent = title;
+    template.querySelector("[data-field='years']").textContent = year;
+    
+    tbody.appendChild(template);
+    
+    form.reset();
 });
